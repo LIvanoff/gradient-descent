9251129a667ec13dc86a507b38ca7daef6733153
LIvanoff <drfrily@gmail.com>
2023-02-16 00:03:43 +0300

    update main

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from freader.fread import fread
 from visualization.visualizator import visualizator
+import subprocess
+
+program = "C:/Users/drfri/source/repos/Optimizer/x64/Debug/Optimizer.exe"
+process = subprocess.Popen(program)
+code = process.wait()
 
 visualizator(fread('./coordinates.txt'))
